7f14624ae96b9f4fab6adff9935c89c8d4b2f567
RXTCoding <60244212+RXTCoding@users.noreply.github.com>
2021-05-05 22:55:42 -0700

    toolbar in the background

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -1,25 +1,23 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 // import { makeStyles } from '@material-ui/core/styles';
 
+class MainBody extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-
-class MainBody extends Component{
-    constructor(){
-        super()
-        this.state={
-
-        }
-    }
-
-    render(){
-        return(
-            <div className="mainBody">
-            <h1 >Grayscale</h1>
-            <p>A free, responsive, one page Bootstrap theme created by Start Bootstrap.</p>
-            
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="mainBody">
+        <h1>Grayscale</h1>
+        <p>
+          A free, responsive, one page Bootstrap theme created by Start
+          Bootstrap.
+        </p>
+      </div>
+    );
+  }
 }
 
-export default MainBody
+export default MainBody;
